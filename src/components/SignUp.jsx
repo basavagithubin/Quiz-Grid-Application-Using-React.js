ae6a1a2461cedd212d7da1be168b1b9bc509ae60
Basavaraj <107114521+basavagithubin@users.noreply.github.com>
2023-12-27 11:59:21 +0530

    Add files via upload

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,73 @@
+// Import React, React Router, and any other necessary dependencies
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Your Home component (Home.jsx)
+const Home = () => {
+  // Your Home component logic goes here
+  console.log('Home.jsx file is running!');
+};
+
+// Your main component (App.jsx)
+const App = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+          <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">
+              Sign in
+            </h2>
+            <p className="mt-2 text-sm text-gray-600">
+              No have an account?{' '}
+              <Link
+                to="/home" // Specify the route to your Home component
+                className="font-semibold text-black transition-all duration-200 hover:underline"
+              >
+                Create a free account
+              </Link>
+            </p>
+            {/* ... (rest of your JSX code) */}
+            <div>
+              <button
+                type="button"
+                className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+              >
+                <Link to="/home" className="text-white no-underline">
+                  Get started
+                </Link>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="ml-2"
+                >
+                  <line x1="5" y1="12" x2="19" y2="12"></line>
+                  <polyline points="12 5 19 12 12 19"></polyline>
+                </svg>
+              </button>
+            </div>
+            {/* ... (rest of your JSX code) */}
+          </div>
+        </div>
+        <div className="h-full w-full">
+          <img
+            className="mx-auto h-full w-full rounded-md object-cover"
+            src="https://images.unsplash.com/photo-1630673245362-f69d2b93880e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1740&amp;q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default App;
+
+
